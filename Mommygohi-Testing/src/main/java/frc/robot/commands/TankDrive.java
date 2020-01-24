d9cf3a4f48da2b2d3aa238402ef3c8ec4f28cdb7
Mommygohi <46762402+Mommygohi@users.noreply.github.com>
2020-01-23 16:30:33 -0800

    Add files via upload

---
+++ Mommygohi-Testing/src/main/java/frc/robot/commands/TankDrive.java
@@ -0,0 +1,54 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import frc.robot.RobotMap;
+
+public class TankDrive extends Command {
+  public TankDrive() {
+    // Use requires() here to declare subsystem dependencies
+    requires(Robot.driveTrain);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    double leftStickY = Robot.m_oi.GetDriverRawAxis(RobotMap.LEFT_STICK_Y);
+    double rightStickY = Robot.m_oi.GetDriverRawAxis(RobotMap.RIGHT_STICK_Y);
+
+    Robot.driveTrain.setLeftMotor(leftStickY); //multiply by a number like .5 for less sensitive movement
+    Robot.driveTrain.setRightMotor(rightStickY);
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+    Robot.driveTrain.setLeftMotor(0); //Just incase it doesn't stop regularly
+    Robot.driveTrain.setRightMotor(0);
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+    this.end();
+  }
+}
