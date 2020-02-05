756657bd836b87d25a04fbc757d081b798e464e9
Mommygohi <46762402+Mommygohi@users.noreply.github.com>
2020-02-05 15:57:58 -0800

    Create Robot.java

---
+++ Mommygohi/Robot.java
@@ -0,0 +1,45 @@
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Robot extends TimedRobot {
+
+    @Override
+    public void robotInit() {
+        // This is called once when the robot code initializes
+    }
+
+    @Override
+    public void robotPeriodic() {
+        // This is called every period regardless of mode
+    }
+
+    @Override
+    public void autonomousInit() {
+        // This is called once when the robot first enters autonomous mode
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+        // This is called periodically while the robot is in autonomous mode
+    }
+
+    @Override
+    public void teleopInit() {
+        // This is called once when the robot first enters teleoperated mode
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        // This is called periodically while the robot is in teleopreated mode
+    }
+
+    @Override
+    public void testInit() {
+        // This is called once when the robot enters test mode
+    }
+
+    @Override
+    public void testPeriodic() {
+        // This is called periodically while the robot is in test mode
+    }
+
+}
