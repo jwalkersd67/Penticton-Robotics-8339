3beb959c1d53149d0abd232dd83a90e4bd46c563
Mommygohi <46762402+Mommygohi@users.noreply.github.com>
2020-02-05 16:25:35 -0800

    Create DriveTrain.java

---
+++ Mommygohi/subsystems/DriveTrain.java
@@ -0,0 +1,25 @@
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.robot.RobotMap;
+
+public class DriveTrain extends Subsystem {
+  Talon leftFrontMotor, rightFrontMotor, leftBackMotor, rightBackMotor;
+  RobotDrive drive;
+  
+  public DriveTrain() {
+    leftFrontMotor = new Talon(RobotMap.leftFrontTalon);
+    rightFrontMotor = new Talon(RobotMap.rightFrontTalon);
+    leftBackMotor = new Talon(RobotMap.leftBackTalon);
+    rightBackMotor = new Talon(RobotMap.rightBackTalon);
+    
+    drive = new RobotDrive(leftFrontMotor, leftBackMotor, rightFrontMotor, rightBackMotor);
+  }
+  
+  public void arcadeDrive(double x, double y){
+    drive.arcadeDrive(x, y)
+  }
+  
+  public void iniDefaultCommand(){
+    setDefaultCommand(new DriveTrainCommand());
+  }
+}
