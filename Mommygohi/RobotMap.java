04c7c9226f069f5cb3a1b4317f79aa740e46a135
Mommygohi <46762402+Mommygohi@users.noreply.github.com>
2020-02-05 17:40:13 -0800

    Delete RobotMap.java

--- Mommygohi/RobotMap.java
+++
@@ -1,8 +0,0 @@
-//package ?;
-
-public class RobotMap {
-  public static final int rightFrontTalon = 0;
-  public static final int leftFrontTalon = 1;
-  public static final int rightBackTalon = 2;
-  public static final int leftBackTalon = 3;
-}
