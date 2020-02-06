8029ff2e29e0d4d4f4642f6bc50b4eb06a7b18d2
Mommygohi <46762402+Mommygohi@users.noreply.github.com>
2020-02-05 16:28:44 -0800

    Create RobotMap.java

---
+++ Mommygohi/RobotMap.java
@@ -0,0 +1,8 @@
+//package ?;
+
+public class RobotMap {
+  public static final int rightFrontTalon = 0;
+  public static final int leftFrontTalon = 1;
+  public static final int rightBackTalon = 2;
+  public static final int leftBackTalon = 3;
+}
