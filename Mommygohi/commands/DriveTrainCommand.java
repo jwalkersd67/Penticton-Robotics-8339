c4e8496933a878ced56fe43636a07fee58a0cb1f
Mommygohi <46762402+Mommygohi@users.noreply.github.com>
2020-02-05 16:50:30 -0800

    Update DriveTrainCommand.java

--- Mommygohi/commands/DriveTrainCommand.java
+++ Mommygohi/commands/DriveTrainCommand.java
@@ -1,1 +1,30 @@
+//package ?;
 
+import edu.wpi.first.wpilibj.command.Command;
+
+public class DriveTrainCommand extends Command {
+  
+  public DriveTrainCommand(){
+    requires(Robot.DriveTrainSub);
+  }
+  
+  protected void initialize(){
+    
+  }
+  
+  protected void execute(){
+    
+  }
+  
+  protected boolean isFinished(){
+    return false;
+  }
+  
+  protected void end(){
+    
+  }
+  
+  protected void interrupted(){
+    
+  }
+}
